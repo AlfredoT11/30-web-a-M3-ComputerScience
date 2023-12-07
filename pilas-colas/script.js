d491bd129bb2ece93861150908b6b716b6ef3f80
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2023-12-06 21:12:37 -0600

    Se implementa una tabla hash con el operador módulo

--- pilas-colas/script.js
+++ pilas-colas/script.js
@@ -66,3 +66,79 @@ if(pilaNodo1.mostrarTope() === null){
 }else{
     console.log(pilaNodo1.mostrarTope());
 }
+
+//Tabla Hash
+
+class TablaHashResiduo{
+
+    constructor(n){
+        this.n = n;
+        this.tabla = new Array(n);
+    }
+
+    // Regresa el índice donde se debe almacenar el valor de entrada.
+    // En este caso utilizamos la función módulo (%)
+    hash(valor){
+        return valor % this.n;
+    }
+
+    insertar(valor){
+        let indice = this.hash(valor);
+
+        if(this.tabla[indice] === undefined){
+            this.tabla[indice] = [];
+        }
+
+        this.tabla[indice].push(valor);
+    }
+
+    buscar(valor){
+        let indice = this.hash(valor);
+
+        if(this.tabla[indice] === undefined){
+            return false;
+        }
+
+        for(let i = 0; i < this.tabla[indice].length; i++){
+            if(this.tabla[indice][i] === valor){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    mostrarTabla(){
+        for(let i = 0; i < this.n; i++){
+            console.log(`${i} : ${this.tabla[i]}`);
+        }
+    }
+
+}
+
+let tablaHash = new TablaHashResiduo(13);
+
+tablaHash.insertar(7);
+tablaHash.insertar(32);
+tablaHash.insertar(14);
+tablaHash.insertar(1000000);
+
+for(let i = 0; i < 100; i++){
+    tablaHash.insertar(Math.floor(Math.random() * 1000));
+}
+
+tablaHash.mostrarTabla();
+
+let valorABuscar = 1000000;
+if(tablaHash.buscar(valorABuscar)){
+    console.log(`El valor ${valorABuscar} SÍ está en la tabla`);
+}else{
+    console.log(`El valor ${valorABuscar} NO está en la tabla`);
+}
+
+valorABuscar = 42;
+if(tablaHash.buscar(valorABuscar)){
+    console.log(`El valor ${valorABuscar} SÍ está en la tabla`);
+}else{
+    console.log(`El valor ${valorABuscar} NO está en la tabla`);
+}
